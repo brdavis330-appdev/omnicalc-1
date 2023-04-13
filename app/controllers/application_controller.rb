46200404abb24892dc2b86bc4ad499cc9592d865
brdavis330 _ <brandimarie4259@gmail.com>
2023-04-13 19:39:57 +0000

    added square results added square results, added form button. added a param and made it an integer number that you can multiple

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   def blank_square_form
     render({ :template => "calculation_template/square_form.html.erb" })
   end
+
+  def calculate_square
+    # prams = {"elephant"=>"42"}
+
+    @num = params.fetch("elephant").to_i
+    @square_of_num = @num ** 2
+    render({ :template => "calculation_template/square_results.html.erb" })
+  end
 end
