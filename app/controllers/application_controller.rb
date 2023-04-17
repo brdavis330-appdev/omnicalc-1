cc5eb2c14f50b0d09825c5e38d06d89af721271c
brdavis330 _ <brandimarie4259@gmail.com>
2023-04-17 16:39:34 +0000

    everything is complete random, sqaure, sqaureroot, and payment is complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,4 +22,33 @@ class ApplicationController < ActionController::Base
 
     render({ :template => "calculation_template/random_results.html.erb" })
   end
+
+  def root_new
+    render({ :template => "calculation_template/square_root_form.html.erb" })
+  end
+
+  def calculate_root
+    @num = params.fetch("user_number").to_f
+    @square_root_of_num = @num ** 0.5
+
+    render({ :template => "calculation_template/square_root_results.html.erb" })
+  end
+
+  def payment_new
+    render({ :template => "calculation_template/payment_form.html.erb" })
+  end
+
+  def calculate_payment
+    @apr = params.fetch("user_apr").to_f
+    @years = params.fetch("user_years").to_f
+    @principal = params.fetch("user_pv").to_f
+
+    n = @years * 12
+    r = @apr / 100 / 12
+    @numerator = r * @principal
+    @denominator = 1 - (1 + r) ** -n
+    @payment = @numerator / @denominator
+
+    render({ :template => "calculation_template/payment_results.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,12 @@ Rails.application.routes.draw do
   get("/random/new", { :controller => "application", :action => "random_new" })
 
   get("/random/results", { :controller => "application", :action => "calculate_random" })
+
+  get("/square_root/new", { :controller => "application", :action => "root_new" })
+
+  get("/square_root/results", { :controller => "application", :action => "calculate_root" })
+
+  get("/payment/new", { :controller => "application", :action => "payment_new" })
+
+  get("/payment/results", { :controller => "application", :action => "calculate_payment" })
 end
